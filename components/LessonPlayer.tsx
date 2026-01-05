1b306e5dfe24bc2d85e93221d23b125f144ddd1d
Menusha Thathsara <161927072+Menusha2k1@users.noreply.github.com>
2026-01-05 19:53:39 +0530

    Create lesson player

---
+++ components/LessonPlayer.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+const LessonPlayer = () => {
+  return (
+    <View>
+      <Text>LessonPlayer</Text>
+    </View>
+  );
+};
+
+export default LessonPlayer;
+
+const styles = StyleSheet.create({});
